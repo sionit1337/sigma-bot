e9193ca4c11b3d8bc8b3c01bafd73b9e6b4d1df9
sionit_1337 <128901310+sionit1337@users.noreply.github.com>
2023-11-10 00:04:30 +0300

    Update mod.py (still buggy and dont work)

--- NewBot/cogs/mod.py
+++ NewBot/cogs/mod.py
@@ -7,12 +7,16 @@ class Mod(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-        @commands.slash_command(name="кик", description="Выгоняет выбранного пользователя с сервера")
+        @commands.slash_command(name="kick", description="Выгоняет выбранного пользователя с сервера")
         async def kick(self, ctx,
-                       target: discord.Option(name="пользователь", type=discord.OptionType.user, description="Пользователь для кика", required=False),
-                       reason: discord.Option(name="причина", type=discord.OptionType.string, description="Причина для кика", required=False)):
+                       target: discord.Option(name="target", type=discord.OptionType.user, description="Пользователь для кика", required=False),
+                       reason: discord.Option(name="reason", type=discord.OptionType.string, description="Причина для кика", required=False)):
             if not ctx.author.guild_permissions.kick_members:
                 await ctx.send(embed=discord.Embed(title="У вас недостаточно прав для этого!", color=0xff0000), ephemeral=True)
+                return
+
+            if target.guild_permissions.kick_members:
+                await ctx.send(embed=discord.Embed(title="Вы не можете выгнать модератора!", color=0xff0000), ephemeral=True)
                 return
 
             if target == ctx.author:
@@ -28,16 +32,20 @@ class Mod(commands.Cog):
                 await ctx.send(embed=discord.Embed(title="Что-то пошло не так", description=f"``{e}``", color=0xff0000), ephemeral=True)
 
 
-        @commands.slash_command(name="мут", description="Запрещает выбранному пользователю писать на сервере")
+        @commands.slash_command(name="mute", description="Запрещает выбранному пользователю писать на сервере")
         async def mute(self, ctx,
                        target: discord.Member,
-                       days: discord.Option(name="дни", type=int, description="Кол-во дней мута", required=False, default=0, max_value=27),
-                       hours: discord.Option(name="часы", type=int, description="Кол-во часов мута", required=False, default=0),
-                       minutes: discord.Option(name="минуты", type=int, description="Кол-во минут мута", required=False, default=0),
-                       seconds: discord.Option(name="секунды", type=int, description="Кол-во секунд мута", required=True, default=0),
-                       reason: discord.Option(name="причина", type=discord.OptionType.string, description="Причина для мута", required=False)):
+                       days: discord.Option(name="days", type=int, description="Кол-во дней мута", required=False, default=0, max_value=27),
+                       hours: discord.Option(name="hours", type=int, description="Кол-во часов мута", required=False, default=0),
+                       minutes: discord.Option(name="minutes", type=int, description="Кол-во минут мута", required=False, default=0),
+                       seconds: discord.Option(name="seconds", type=int, description="Кол-во секунд мута", required=True, default=0),
+                       reason: discord.Option(name="reason", type=discord.OptionType.string, description="Причина для мута", required=False)):
             if not ctx.author.guild_permissions.mute_members:
                 await ctx.send(embed=discord.Embed(title="У вас недостаточно прав для этого!", color=0xff0000), ephemeral=True)
+                return
+
+            if target.guild_permissions.moderate_members:
+                await ctx.send(embed=discord.Embed(title="Вы не можете замутить модератора!", color=0xff0000), ephemeral=True)
                 return
 
             if target == ctx.author:

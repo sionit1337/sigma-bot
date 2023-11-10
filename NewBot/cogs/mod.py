49b43dd94369725c326ba7ba1986b192aa623f45
sionit_1337 <128901310+sionit1337@users.noreply.github.com>
2023-11-10 21:13:00 +0300

    Updated mod.py (still...)

--- NewBot/cogs/mod.py
+++ NewBot/cogs/mod.py
@@ -7,49 +7,95 @@ class Mod(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-        @commands.slash_command(name="kick", description="Выгоняет выбранного пользователя с сервера")
-        async def kick(self, ctx,
-                       target: discord.Option(name="target", type=discord.OptionType.user, description="Пользователь для кика", required=False),
-                       reason: discord.Option(name="reason", type=discord.OptionType.string, description="Причина для кика", required=False)):
+        @commands.slash_command(name="kick",
+                                description="Выгоняет выбранного пользователя с сервера",
+                                options=[discord.Option(name="target",
+                                                        type=discord.OptionType.user,
+                                                        description="Пользователь для кика",
+                                                        required=True),
+                                         discord.Option(name="reason",
+                                                        type=discord.OptionType.string,
+                                                        description="Причина для кика",
+                                                        required=False)])
+        async def kick(self,
+                       ctx,
+                       target: discord.Member,
+                       reason: str):
             if not ctx.author.guild_permissions.kick_members:
-                await ctx.send(embed=discord.Embed(title="У вас недостаточно прав для этого!", color=0xff0000), ephemeral=True)
+                await ctx.send(embed=discord.Embed(title="У вас недостаточно прав для этого!", color=0xff0000),
+                               ephemeral=True)
                 return
 
             if target.guild_permissions.kick_members:
-                await ctx.send(embed=discord.Embed(title="Вы не можете выгнать модератора!", color=0xff0000), ephemeral=True)
+                await ctx.send(embed=discord.Embed(title="Вы не можете выгнать модератора!", color=0xff0000),
+                               ephemeral=True)
                 return
 
             if target == ctx.author:
-                await ctx.send(embed=discord.Embed(title="Вы не можете выгнать самого себя!", color=0xff0000), ephemeral=True)
+                await ctx.send(embed=discord.Embed(title="Вы не можете выгнать самого себя!", color=0xff0000),
+                               ephemeral=True)
                 return
 
             try:
                 await target.kick(reason=reason)
 
-                await ctx.send(embed=discord.Embed(title=f"{target.display_name} изгнан по причине ``{reason}``", color=0xffbb00))
+                await ctx.send(
+                    embed=discord.Embed(title=f"{target.display_name} изгнан по причине ``{reason}``", color=0xffbb00))
+
+                print(msg=f"{target.display_name} был изгнан")
 
             except Exception as e:
-                await ctx.send(embed=discord.Embed(title="Что-то пошло не так", description=f"``{e}``", color=0xff0000), ephemeral=True)
+                await ctx.send(embed=discord.Embed(title="Что-то пошло не так", description=f"``{e}``", color=0xff0000),
+                               ephemeral=True)
 
-
-        @commands.slash_command(name="mute", description="Запрещает выбранному пользователю писать на сервере")
-        async def mute(self, ctx,
+        @commands.slash_command(name="mute",
+                                description="Запрещает выбранному пользователю писать на сервере",
+                                options=[discord.Option(name="target",
+                                                        type=discord.OptionType.user,
+                                                        description="Пользователь для мута",
+                                                        required=True),
+                                         discord.Option(name="days",
+                                                        type=discord.OptionType.integer,
+                                                        description="Кол-во дней мута",
+                                                        required=False,
+                                                        max_value=27),
+                                         discord.Option(name="hours",
+                                                        type=discord.OptionType.integer,
+                                                        description="Кол-во часов мута",
+                                                        required=False),
+                                         discord.Option(name="minutes",
+                                                        type=discord.OptionType.integer,
+                                                        description="Кол-во минут мута",
+                                                        required=False),
+                                         discord.Option(name="seconds",
+                                                        type=discord.OptionType.integer,
+                                                        description="Кол-во секунд мута",
+                                                        required=True),
+                                         discord.Option(name="reason",
+                                                        type=discord.OptionType.string,
+                                                        description="Причина для мута",
+                                                        required=False)])
+        async def mute(self,
+                       ctx,
                        target: discord.Member,
-                       days: discord.Option(name="days", type=int, description="Кол-во дней мута", required=False, default=0, max_value=27),
-                       hours: discord.Option(name="hours", type=int, description="Кол-во часов мута", required=False, default=0),
-                       minutes: discord.Option(name="minutes", type=int, description="Кол-во минут мута", required=False, default=0),
-                       seconds: discord.Option(name="seconds", type=int, description="Кол-во секунд мута", required=True, default=0),
-                       reason: discord.Option(name="reason", type=discord.OptionType.string, description="Причина для мута", required=False)):
+                       days: int = 0,
+                       hours: int = 0,
+                       minutes: int = 0,
+                       seconds: int = 1,
+                       reason: str = None):
             if not ctx.author.guild_permissions.mute_members:
-                await ctx.send(embed=discord.Embed(title="У вас недостаточно прав для этого!", color=0xff0000), ephemeral=True)
+                await ctx.send(embed=discord.Embed(title="У вас недостаточно прав для этого!", color=0xff0000),
+                               ephemeral=True)
                 return
 
             if target.guild_permissions.moderate_members:
-                await ctx.send(embed=discord.Embed(title="Вы не можете замутить модератора!", color=0xff0000), ephemeral=True)
+                await ctx.send(embed=discord.Embed(title="Вы не можете замутить модератора!", color=0xff0000),
+                               ephemeral=True)
                 return
 
             if target == ctx.author:
-                await ctx.send(embed=discord.Embed(title="Вы не можете замутить самого себя!", color=0xff0000), ephemeral=True)
+                await ctx.send(embed=discord.Embed(title="Вы не можете замутить самого себя!", color=0xff0000),
+                               ephemeral=True)
                 return
 
             try:
@@ -57,11 +103,15 @@ class Mod(commands.Cog):
 
                 await target.timeout(duration=duration, reason=reason)
 
-                await ctx.send(embed=discord.Embed(title=f"{target.display_name} замьючен на {days} дней, {hours} часов, {minutes} минут и {seconds} секунд", color=0xffbb00))
+                await ctx.send(embed=discord.Embed(
+                    title=f"{target.display_name} замьючен на {days} дней, {hours} часов, {minutes} минут и {seconds} секунд",
+                    color=0xffbb00))
+
+                print(msg=f"{target.display_name} замьючен на {days}d:{hours}h:{minutes}m:{seconds}s")
 
             except Exception as e:
-                await ctx.send(embed=discord.Embed(title="Что-то пошло не так", description=f"``{e}``", color=0xff0000), ephemeral=True)
-
+                await ctx.send(embed=discord.Embed(title="Что-то пошло не так", description=f"``{e}``", color=0xff0000),
+                               ephemeral=True)
 
 
 def setup(bot: commands.Bot):
